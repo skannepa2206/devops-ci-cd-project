8a2cded2859f3c3862a875bb84e0e12cab1512b2
skannepa2206 <skannepa2206@gmail.com>
2025-03-09 18:51:23 -0400

    Test: Update home message for pipeline testing

--- app/main.py
+++ app/main.py
@@ -4,12 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-#   return "🚀 Application is running successfully!"
     return "🚀 DevOps Automation in Action!"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
 
-#Test2
+#Test1
