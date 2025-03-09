6c5540f4756c4718ce713f61b54f0ab3c4eadb2b
skannepa2206 <skannepa2206@gmail.com>
2025-03-09 14:37:04 -0400

    Updated homepage message

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Application is running successfully!"
+#   return "🚀 Application is running successfully!"
+    return "🚀 DevOps Automation in Action!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
