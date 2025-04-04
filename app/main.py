fe29a4acd5bc8a384598f02dc2ed71ec3878834f
skannepa2206 <skannepa2206@gmail.com>
2025-04-04 17:49:46 -0400

    Update web app message

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 DevOps Automation in Action YaaY!"
+    return "🚀 DevOps Automation in Action YaaY- Update1!"
 
 if __name__ == "__main__":
     # Explicitly bind to all interfaces (0.0.0.0) and port 5000
